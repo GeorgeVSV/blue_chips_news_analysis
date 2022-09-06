98b032f55eede69609723c8e75d0c9cdf974a8a5
GeorgeVSV <112613534+GeorgeVSV@users.noreply.github.com>
2022-09-06 17:18:43 +0300

    Delete tester.py

--- tester.py
+++
@@ -1,9 +0,0 @@
-from configparser import ConfigParser
-import re
-
-conf = ConfigParser()
-conf.read('config/cnf.ini')
-#print(conf['website_url']['usr_agent'])
-
-
-print(re.sub(r',.*', "", 'Polymetal, облигация'))
