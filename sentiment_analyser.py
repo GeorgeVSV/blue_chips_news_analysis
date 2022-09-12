649fb4f42cc73930eb6df22381efd24e482311c6
GeorgeVSV <vosorovg@gmail.com>
2022-09-12 12:04:14 +0300

    Add class SentimentProcessing with attribute "df_cips"(which stores all chips data inside) in class constructor, it'll used for computation economy in future. Also add class method "score_news_polarity" which perform sentiment analysis on chips data from "df_chips" attribute and return pd.DataFrame prepared for further visualization

--- sentiment_analyser.py
+++ sentiment_analyser.py
@@ -1,49 +1,40 @@
-import json
+import matplotlib
+import matplotlib.pyplot as plt
 import pandas as pd
 from nltk.sentiment import SentimentIntensityAnalyzer
-import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
+from scrap import FinScrap
 
 
+class SentimentProcessing:
+    def __init__(self):
+        scraper = FinScrap()
+        df_dict = scraper.get_chips_news()
+        self.df_chips = pd.DataFrame(df_dict, columns=['Stock', 'Date', 'Title'])
 
+    def score_news_polarity(self) -> pd.DataFrame:
+        """
+        Calculate polarity scores of news and parse the data in appropriate format for visualization
 
-# Data preparation for raw titles from parser
-
-class PolarityPrep:
-    #def __init__(self):
-        #self.df_chips =
-    file_jsn = open('result.json')
-    chips = json.load(file_jsn)
-    s = pd.DataFrame(chips, columns=['Stock', 'Date', 'Title'])
-    vader = SentimentIntensityAnalyzer()
-
-    scores = s['Title'].apply(vader.polarity_scores).tolist()
-    # Convert the 'scores' list of dicts into a DataFrame
-    scores_df = pd.DataFrame(scores)
-    # Join the DataFrames of the news and the list of dicts
-    parsed_and_scored_news = s.join(scores_df, rsuffix='_right')
-    parsed_and_scored_news.Date = pd.to_datetime(parsed_and_scored_news.Date, infer_datetime_format=True)
-
-    mean_scores = parsed_and_scored_news.groupby(['Stock', 'Date']).mean()
-    # Unstack the column ticker
-    mean_scores = mean_scores.unstack()
-    # Get the cross-section of compound in the 'columns' axis
-
-    mean_scores = mean_scores.xs('compound', axis="columns").transpose()
-
-
-
-
-
-
-
-
-
+        :return: pd.DataFrame with polairty scores for all blue chips with separation based on days
+        """
+        vader = SentimentIntensityAnalyzer()
+        scores = self.df_chips['Title'].apply(vader.polarity_scores).tolist()
+        # Convert the 'scores' list of dicts into a DataFrame
+        scores_df = pd.DataFrame(scores)
+        # Join the DataFrames of the news and the list of dicts
+        parsed_and_scored_news = scores_df.join(scores_df, rsuffix='_right')
+        parsed_and_scored_news.Date = pd.to_datetime(parsed_and_scored_news.Date, infer_datetime_format=True)
+        # Group polarit score by Stock name and date
+        mean_scores = parsed_and_scored_news.groupby(['Stock', 'Date']).mean()
+        # Unstack the column 'stock'
+        mean_scores = mean_scores.unstack()
+        # Get the cross-section of compound in the 'columns' axis
+        mean_scores = mean_scores.xs('compound', axis="columns").transpose()
+        return mean_scores
 
 
 # Time period selection for plotting
-
+'''
 # Let's make average on month
 avg_month = mean_scores.groupby(pd.Grouper(freq='W')).mean()
 avg_month = avg_month[avg_month != 0]
@@ -57,6 +48,7 @@ avg_month = avg_month.set_index(avg_month.reset_index().Date.apply(lambda t: t.s
 
 
 # Plot graphs
+matplotlib.use('TkAgg')
 interese_stock = ['Gazprom', 'Novatek', 'Lukoil']
 
 avg_month[interese_stock].plot(kind= 'bar')
@@ -66,3 +58,4 @@ plt.rcParams['figure.figsize'] = [10, 6]
 ax = plt.gca()
 plt.legend(bbox_to_anchor=(1.1, 1.1), bbox_transform=ax.transAxes)
 plt.show()
+'''
